f4aa06c3649ea19620bfb0dcab17da46f0cdb0a6
danny <danny21316@gmail.com>
2025-11-10 15:32:21 +0800

    fix: name error

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
 const secondsHandDom = document.querySelector('.second-hand');
-const minutesHandDom = document.querySelector('min-hand');
+const minutesHandDom = document.querySelector('.min-hand');
 const hourHandDom = document.querySelector('.hour-hand');
 function setDate() {
     const now = new Date();
@@ -9,12 +9,13 @@ function setDate() {
     secondsHandDom.style.transform = `rotate(${secondsDegrees}deg)`
 
     const mins = now.getMinutes();
-    const minsDegress = ((mins / 60) * 360) + 90;
-    minutesHandDom.style.transform = `rotate(${minsDegress}deg)`
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minutesHandDom.style.transform = `rotate(${minsDegrees}deg)`
 
     const hours = now.getMinutes();
-    const hoursDegress = ((hours / 12) * 360) + 90;
-    hourHandDom.style.transform = `rotate(${hoursDegress}deg)`
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHandDom.style.transform = `rotate(${hoursDegrees}deg)`
 }
 
+setDate(); 
 setInterval(setDate, 1000);
